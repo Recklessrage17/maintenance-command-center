abd47bb6b60542fe3dc63a94d9515d33689e8ce4
Recklessrage17 <masterjrgrove.jg@gmail.com>
2026-07-01 04:20:51 -0500

    Work PC

--- Start MCC Website.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set PORT=4273
-start "Maintenance Command Center" cmd /k "npm start --prefix backend"
-timeout /t 2 /nobreak >nul
-start "" "http://localhost:4273"
-endlocal
--- Stop MCC Website.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set MCC_PORT=4273
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr /R /C:":%MCC_PORT% .*LISTENING"') do (
-  echo Stopping process %%a using port %MCC_PORT%...
-  taskkill /PID %%a /F
-)
-echo Maintenance Command Center stop command completed for port %MCC_PORT% only.
-endlocal
