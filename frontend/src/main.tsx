70f62edb1c978aca465d36a4cff98d1c4ae2b69b
Recklessrage17 <masterjrgrove.jg@gmail.com>
2026-07-08 11:42:37 -0500

    Wire measurement log UI refinement

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './styles/app.css';
 import './modules/machine-library/measurementInspectionLogsDomPatch';
 import './modules/machine-library/measurementInspectionLogsEnhancements';
 import './modules/machine-library/measurementInspectionPrintOverride';
+import './modules/machine-library/measurementInspectionUiRefinement';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
