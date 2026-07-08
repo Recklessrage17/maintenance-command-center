964bb4fe6d64bdee15c6050c7952419501aeba24
Recklessrage17 <masterjrgrove.jg@gmail.com>
2026-07-08 09:04:46 -0500

    Wire measurement log enhancements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/app.css';
 import './modules/machine-library/measurementInspectionLogsDomPatch';
+import './modules/machine-library/measurementInspectionLogsEnhancements';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
