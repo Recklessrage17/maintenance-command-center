fc15614ecd86e31c02fe971783696aaba2dab36e
Recklessrage17 <masterjrgrove.jg@gmail.com>
2026-07-28 06:24:34 -0500

    Load command-deck login theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/app.css';
 import './styles/glass-system.css';
+import './styles/login-command-deck.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
